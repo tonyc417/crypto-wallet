2c9edaf0d954b38180c00110bf90fd9890ddd76b
unknown <tonycruz417@gmail.com>
2019-05-27 18:59:03 -0500

    added bitcoin price

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import React,{useEffect, useState} from 'react';
 import Trading from './components/trading';
+import Bitcoin from './components/bitcoin';
 import './App.css';
 
- const App = () => {
-  const API_KEY = '';
+const App = () => {
 
-  const [ticker, setInfo] = useState([]);
+ const API_KEY = '';
 
-  useEffect(() => {
-    getData();
-  }, []);
+ const [ticker, setInfo] = useState([]);
 
-   const getData = async () => {
-     const response = await fetch(`https://cloud.iexapis.com/stable/stock/aapl/quote?token=${API_KEY}`);
-     const data = await response.json();
-     setInfo(data);
-     console.log(data);
-   }
-  return (
-    <div className="App">
-      <h1>Watchlist</h1>
-      <Trading title={ticker.companyName} price={ticker.high} volume={ticker.latestVolume}/>
-    </div>
-  );
+ useEffect(() => {
+  //  getData();
+  getBit();
+ }, []);
+
+  const getData = async () => {
+    const response = await fetch(`https://cloud.iexapis.com/stable/stock/aapl/quote?token=${API_KEY}`);
+    const data = await response.json();
+    setInfo(data);
+    console.log(data);
+  }
+
+  const getBit = async () => {
+    const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+ return (
+
+   <div>
+     <Nav />
+     <Trading title={ticker.companyName} price={ticker.high} volume={ticker.latestVolume}/>
+   </div>
+ );
 }
 
-// function PageHead() {
-//   return(
-//     <div className="App">
-//       <h1>Stock Wallet</h1>
-//       <p>Apple Quote:</p>
-//       <p>Latest XRP Price:</p>
-//     </div>
-//   );
-// }
+const Nav = () => {
+ return(
+   <div>
+     <ul className="navBar">
+       <li>Home</li>
+       <li>News</li>
+       <li>Test</li>
+     </ul>
+     <h1>Stock Wallet</h1>
+     <h1>Watchlist</h1>
+   </div>
+ )
+}
 
 export default App;
---
+++ src/components/bitcoin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bit = (priceBit) => {
+    return(
+        <div>
+            <h2>Bitcoin Watch</h2>
+            <p>The Current Bitcoin Price is: {priceBit} </p>
+        </div>
+    )
+}
+
+export default Bit;
