5863a34524111bedb85b98d09160903efc1adfa7
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-06-30 13:25:41 -0500

    added a navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const navbar = () => {
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About</a></li>
-                <li><a href="/">Blog</a></li>
+                <li><a href="/">Stocks</a></li>
                 <li><a href="/">Contact</a></li>
             </ul>
         </nav>
