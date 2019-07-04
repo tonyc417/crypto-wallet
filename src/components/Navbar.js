4181ef2bd8539ae1c93f0c5d57e00aa1419b0180
unknown <tonycruz417@gmail.com>
2019-07-04 15:48:57 -0500

    working on top bar for markets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,82 @@
 import React from 'react';
 
 const navbar = () => {
-    return(
+    return (
         <div>
             <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">About</a></li>
-                <li><a href="/">Stocks</a></li>
-                <li><a href="/">Contact</a></li>
-            </ul>
-        </nav>
+                <ul>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>NASDAQ 100</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>DOW</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>S&P 500</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>EUR/USD</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>EUR/USD</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>EUR/USD</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            <div>
+                                <span>EUR/USD</span>
+                            </div>
+                            <div>
+                                <span>0.77%</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
