60b8e2c17533cd93593ceb621ca396709cd2b660
unknown <tonycruz417@gmail.com>
2019-07-04 15:12:44 -0500

    searching now works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Trading from './components/trading';
 import Bitcoin from './components/bitcoin';
 import Navbar from './components/Navbar';
-import Welcome from './components/welcome';
 import News from './components/news';
 import './App.css';
 
 const App = () => {
 
- const API_KEY = '';
+  const API_KEY = 'pk_570d6172523a4af8a34f56e226b546cf';
 
- const [ticker, setInfo] = useState([]);
- const [total, setPrice] = useState([]);
+  const [ticker, setInfo] = useState([]);
+  const [total, setPrice] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('TSLA');
 
- useEffect(() => {
-   getData();
-  getBit();
- }, []);
+  useEffect(() => {
+    // getData();
+    getBit();
+  }, [query]);
 
   const getData = async () => {
-    const response = await fetch(`https://cloud.iexapis.com/stable/stock/aapl/quote?token=${API_KEY}`);
+    const response = await fetch(`https://cloud.iexapis.com/stable/stock/${query}/quote?token=${API_KEY}`);
     const data = await response.json();
     setInfo(data);
     console.log(data);
@@ -32,16 +33,34 @@ const App = () => {
     console.log(data.bpi);
   }
 
- return (
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
 
-   <div>
-     <Navbar />
-     <Welcome />    
-     <Trading title={ticker.companyName} price={ticker.high} volume={ticker.latestVolume}/>
-     <Bitcoin priceBit={total.rate}/>
-     <News />
-   </div>
- );
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
+  }
+
+  return (
+
+    <div>
+      <Navbar />
+      <div className="text-center">
+        <div className="jumbotron" >
+          <h1>Welcome, Traders</h1>
+          <form onSubmit={getSearch} className="search-form">
+            <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="Search by symbol" />
+            <button className="search-button" type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+      <Trading title={ticker.companyName} price={ticker.high}
+        volume={ticker.latestVolume} high={ticker.week52High} closing={ticker.close}/>
+      <Bitcoin priceBit={total.rate} />
+      <News />
+    </div>
+  );
 }
 
 
--- src/components/trading.js
+++ src/components/trading.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const StockInfo = ({title,price,volume}) => {
+const StockInfo = ({title,price,volume,high,closing}) => {
     return (
         <div>
             <h3>{title}</h3>
             <p>Highest Price: {price}</p>
             <p>Today's Volume: {volume}</p>
+            <p>52 Week High: ${high}</p>
+            <p>Closing Price: ${closing}</p>
             <h4>About</h4>
             <p></p>
         </div>
