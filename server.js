adc6851a80f0c3930c731f26942f8909e2e55cd6
Tony Cruz <tonycruz417@gmail.com>
2019-07-14 12:16:18 -0500

    can now start with node server

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const path = require('path');
+const cheerio = require('cheerio');
+const generatePassword = require('password-generator');
 
 const app = express();
 
-var PORT = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => {
+    const count = 5;
+
+    const passwords = Array.from(Array(count).keys()).map(i =>
+        generatePassword(12, false))
+        res.json(passwords);
+        console.log(`Sent ${count} passwords`);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log("Server started on port" + PORT));
